9e4297d8c5addfd1d4d41e4a79b43cf2e9f52438
André Prado <andre@pixelcart.com.br>
2022-01-03 13:55:33 -0300

    extraindo dados do site da conceito

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ const cheerio = require ('cheerio')
 
 const app = express()
 
-
-const imoveis = []
-
 app.get('/conceito', (req, res) => {
 
+    let pages = []
+    let imoveis = []
     axios.get('https://www.conceitoimoveispg.com.br/busca/locacao/')
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
-
-
+            $('.pagination> li', html).each(function() {
+                page = $(this).find('a').text()
+                pages.push(page);
+            })
+            last_page = pages[pages.length-2]
         })
+        .then(response => {
+            axios.get('https://www.conceitoimoveispg.com.br/busca/locacao/')
+                .then(response => {
+                    const html = response.data
+                    const $ = cheerio.load(html)
+                    $('.searchItem').each(function() {
 
+                        const title = $(this).find('.imobTitle > div > h2 > strong').text().trim()
+                        const address = $(this).find('.hide').text().trim()
+                        const type = $(this).find('.innerInfo').first().text().trim()
+                        const neighbourhood = $(this).find('.innerInfo').last().text().trim()
+                        const price = $(this).find('.imobPrice').text().trim()
+                        const link = $(this).find('a').attr('href').trim()
+                        imoveis.push({
+                            title
+                            , address
+                            , type
+                            , neighbourhood
+                            , price
+                            , link
+                        })
+                    })
+                    console.log(imoveis.length)
+                    res.json(imoveis)
+                })
+        })
 })
 
 
